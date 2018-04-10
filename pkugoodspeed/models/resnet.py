f49dd17c0937af9f10b98ad2e1a71324b0484e70
Zebo Li <zebo@ginkgo-llc.com>
2018-04-10 08:31:57 -0700

    sdf

--- pkugoodspeed/models/resnet.py
+++ pkugoodspeed/models/resnet.py
@@ -23,36 +23,34 @@ class ResNet:
         
         # build resnet blocks
         n_filters = n_filters
+        dropout_ratio = 0.1
         kernel = BatchNormalization(axis=-1) (Conv2D(filters=n_filters, kernel_size=3, padding="same") (in_layer))
         kernel = Activation('relu') (kernel)
         for _ in range(depth):
             layerA = Conv2D(filters=2*n_filters, kernel_size=3, padding="same") (kernel)
+            layerA = Dropout(dropout_ratio) (layerA)
             layerB = Conv2D(filters=int(n_filters/2), kernel_size=3, padding="same") (kernel)
             layerB = Activation('relu') (BatchNormalization(axis=-1) (layerB))
             layerB = Conv2D(filters=int(n_filters/2), kernel_size=3, padding="same") (layerB)
             layerB = Activation('relu') (BatchNormalization(axis=-1) (layerB))
             layerB = Conv2D(filters=2*n_filters, kernel_size=3, padding="same") (layerB)
+            layerB = Dropout(dropout_ratio) (layerB)
             kernel = merge([layerA, layerB], mode='sum')
             
-            layerB = Activation('relu') (BatchNormalization(axis=-1) (kernel))
-            layerB = Conv2D(filters=int(n_filters/2), kernel_size=3, padding="same") (layerB)
-            layerB = Activation('relu') (BatchNormalization(axis=-1) (layerB))
-            layerB = Conv2D(filters=int(n_filters/2), kernel_size=3, padding="same") (layerB)
-            layerB = Activation('relu') (BatchNormalization(axis=-1) (layerB))
-            layerB = Conv2D(filters=2*n_filters, kernel_size=3, padding="same") (layerB)
-            kernel = merge([kernel, layerB], mode='sum')
-            
-            layerB = Activation('relu') (BatchNormalization(axis=-1) (kernel))
-            layerB = Conv2D(filters=int(n_filters/2), kernel_size=3, padding="same") (layerB)
-            layerB = Activation('relu') (BatchNormalization(axis=-1) (layerB))
-            layerB = Conv2D(filters=int(n_filters/2), kernel_size=3, padding="same") (layerB)
-            layerB = Activation('relu') (BatchNormalization(axis=-1) (layerB))
-            layerB = Conv2D(filters=2*n_filters, kernel_size=3, padding="same") (layerB)
-            kernel = merge([kernel, layerB], mode='sum')
+            for _ in range(3):
+                layerB = Activation('relu') (BatchNormalization(axis=-1) (kernel))
+                layerB = Conv2D(filters=int(n_filters/2), kernel_size=3, padding="same") (layerB)
+                layerB = Activation('relu') (BatchNormalization(axis=-1) (layerB))
+                layerB = Conv2D(filters=int(n_filters/2), kernel_size=3, padding="same") (layerB)
+                layerB = Activation('relu') (BatchNormalization(axis=-1) (layerB))
+                layerB = Conv2D(filters=2*n_filters, kernel_size=3, padding="same") (layerB)
+                layerB = Dropout(min(0.6, 2 * dropout_ratio)) (layerB)
+                kernel = merge([kernel, layerB], mode='sum')
             
             kernel = Activation('relu') (BatchNormalization(axis=-1) (kernel))
             kernel = MaxPooling2D((2, 2)) (kernel)
             n_filters *= 2
+            dropout_ratio = min(0.5, 2*dropout_ratio)
         
         denlayer = GlobalAveragePooling2D() (kernel)
         # denlayer = GlobalMaxPooling2D() (layerA)
