2662a583840374f997e3d1f6abbd4fe6c3ccc5e4
Zebo Li <zebo@ginkgo-llc.com>
2018-04-13 17:39:21 -0700

    making predictions

--- pkugoodspeed/pred_main.py
+++ pkugoodspeed/pred_main.py
@@ -46,33 +46,6 @@ if __name__ == '__main__':
     odim = ip.getOutputDim()
     # get model instance
     model_obj = model_dict[C['model_name']](input_shape=ishape, output_dim=odim)
-
-    ## Creating OOF
-    """
-    kf = KFold(len(df), n_folds=5, random_state=17)
-    for i, (train_index, valid_index) in enumerate(kf):
-        model_obj.buildModel(**C['model_kargs'])
-        print("Starting Fold # {0} OOF ...".format(str(i)))
-        x, y = ip.getbatch(idx=train_index, **C['batch'])
-        print("Input shape:")
-        print(x.shape)
-        print("\nOutput shape:")
-        print(y.shape)
-        valid_x, valid_y = ip.getbatch(idx=valid_index)
-        model = model_obj.getModel()
-        trainer = Trainer(model=model, model_name=C['model_name'])
-        res = trainer.train(x, y, valid_set=(valid_x, valid_y), **C['train_args'])
-        fold_prediction = trainer.predict(valid_x, checker_path=C['train_args']['checker_path'])
-        for j, ans in zip(valid_index, fold_prediction):
-            oof_pred[j] = _encode(ans)
-
-    df['oof_pred'] = oof_pred
-    oof_path = './output/' + C['model_name']
-    if not os.path.exists(oof_path):
-        os.makedirs(oof_path)
-    oof_file = oof_path + "/{LAB}_oof.csv".format(LAB=C["proc"]["category"])
-    df.to_csv(oof_file, index=False)
-    """
     # Training all
     n = len(df)
     n_train = int(n*0.92)
